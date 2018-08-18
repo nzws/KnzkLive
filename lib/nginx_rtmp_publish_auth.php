76125ccddc4555d8928ef27b910d54ee77adb43f
nzws (yuzulabo) <git@nzws.me>
2018-08-18 20:24:45 +0900

    fix #1

--- lib/nginx_rtmp_publish_auth.php
+++ lib/nginx_rtmp_publish_auth.php
@@ -1,16 +1,20 @@
 <?php
 require_once("initload.php");
 $_GET["tcurl"] = s($_GET["tcurl"]);
-if (!$_GET["tcurl"]) {
+if (!$_GET["tcurl"]) { //入力されてない
     http_response_code(404);
     exit();
 }
 $live = getLive(strstr($_GET["name"], 'stream', true));
-if (!$live) {
+if (!$live) { //存在しない
     http_response_code(500);
     exit();
 }
 
-if (strstr($_GET["tcurl"], 'token=') !== "token=" . $live["token"]) {
+if ($live["is_live"] == 0) { //配信終了
+    http_response_code(404);
+}
+
+if (strstr($_GET["tcurl"], 'token=') !== "token=" . $live["token"]) { //認証
     http_response_code(404);
 }
