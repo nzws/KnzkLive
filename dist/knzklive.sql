5267f25068c374deca7a4276844326d9c39b66e0
nzws <github@nzws.me>
2018-10-07 12:26:44 +0900

    fix

--- dist/knzklive.sql
+++ dist/knzklive.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `knzklive`
 --
-CREATE DATABASE IF NOT EXISTS `knzklive` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `knzklive` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `knzklive`;
 
 create table live
