4d89cb7eb7d326e528fdb261a970d56299fda86d
bsky <git@bsky.moe>
2018-12-02 18:55:18 +0900

    Fix code style in JavaScript files

--- public/js/knzklive.js
+++ public/js/knzklive.js
@@ -1,23 +1,23 @@
 function elemId(_id) {
-  return document.getElementById(_id);
+  return document.getElementById(_id)
 }
 
 function escapeHTML(text) {
   text = text
-  .replace(/"/g, '"')
-  .replace(/'/g, "'")
-  .replace(/&/g, '&amp;')
-  .replace(/</g, '&lt;')
-  .replace(/>/g, '&gt;');
+    .replace(/"/g, '"')
+    .replace(/'/g, "'")
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
 
-  return text;
+  return text
 }
 
 function buildQuery(data) {
-  var body = '';
+  var body = ''
   for (var key in data) {
-    body += key + '=' + encodeURIComponent(data[key]) + '&';
+    body += key + '=' + encodeURIComponent(data[key]) + '&'
   }
-  body += 'd=' + new Date().getTime();
-  return body;
+  body += 'd=' + new Date().getTime()
+  return body
 }
--- ws/index.js
+++ ws/index.js
@@ -1,28 +1,30 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
-let Socket;
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const bodyParser = require('body-parser')
+let Socket
 
 app.get('/', function(req, res) {
-  res.send('ok');
-});
+  res.send('ok')
+})
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+app.use(bodyParser.json())
 
 app.post('/send_comment', function(req, res) {
-  console.log('[KnzkLive WebSocket] Send Comment', req.body);
-  io.emit('knzklive_comment_' + req.body.live_id, req.body);
-  res.end();
-});
+  console.log('[KnzkLive WebSocket] Send Comment', req.body)
+  io.emit('knzklive_comment_' + req.body.live_id, req.body)
+  res.end()
+})
 
 io.on('connection', function(socket) {
-  console.log('[KnzkLive WebSocket] connected');
-});
+  console.log('[KnzkLive WebSocket] connected')
+})
 
 http.listen(3000, function() {
-  console.log('[KnzkLive WebSocket] listening on *:3000');
-});
+  console.log('[KnzkLive WebSocket] listening on *:3000')
+})
