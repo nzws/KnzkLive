51686c61a24ad73f2d7f94264a482c7b32e89089
nzws <github@nzws.me>
2019-01-30 07:06:44 +0900

    add favicon

--- include/header.php
+++ include/header.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
       crossorigin="anonymous">
 <link rel="stylesheet" href="<?=$env["RootUrl"]?>style.css?2019-01-18">
+<link rel="shortcut icon" type="image/x-icon" href="<?=$env["RootUrl"]?>favicon.ico">
