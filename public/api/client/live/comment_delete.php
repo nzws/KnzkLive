ed30cc127efda21a977c242b99030dbb51fe867a
nzws <github@nzws.me>
2019-02-06 17:27:03 +0900

    fix mistake

--- public/api/client/live/comment_delete.php
+++ public/api/client/live/comment_delete.php
@@ -7,4 +7,4 @@ if (!$my) api_json(["error" => "エラー: ログインしてください。"]);
 $live = getLive($_POST["live_id"]);
 if (!$live || $live["user_id"] !== $my["id"]) api_json(["error" => "エラー: あなたに編集権限がありません。"]);
 
-api_json(["success" => comment_delete($my["id"], $live["user_id"], s($_POST["delete_id"]), $_POST["is_knzklive"] == 1)]);
+api_json(["success" => comment_delete($my["id"], $live["id"], s($_POST["delete_id"]), $_POST["is_knzklive"] == 1)]);
