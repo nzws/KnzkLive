03d308167ccf5f30fc1175c7422eef3b8cdd3f5f
nzws <github@nzws.me>
2019-02-07 23:32:20 +0900

    fix sql

--- lib/live.php
+++ lib/live.php
@@ -83,7 +83,7 @@ function getAllLive($notId = 0, $is_history = false) {
 
 function getLastLives() {
   $mysqli = db_start();
-  $stmt = $mysqli->prepare("SELECT * FROM live WHERE id in (SELECT max(id) FROM live WHERE privacy_mode = 1 AND is_started = 1 GROUP BY user_id ORDER BY id);");
+  $stmt = $mysqli->prepare("SELECT * FROM live WHERE id in (SELECT max(id) FROM live WHERE privacy_mode = 1 AND is_started = 1 GROUP BY user_id ORDER BY id DESC) ORDER BY id DESC;");
   $stmt->execute();
   $row = db_fetch_all($stmt);
   $stmt->close();
