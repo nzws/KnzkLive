08e90258d61ead98b5e9a874507f17b3735d7125
bsky <git@bsky.moe>
2019-02-27 22:52:10 +0900

    Move favicon.ico into static directory

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <link rel="stylesheet" href="<?=$env["RootUrl"]?>style.css?2019/02/16">
-<link rel="shortcut icon" type="image/x-icon" href="<?=$env["RootUrl"]?>favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="<?=$env["RootUrl"]?>static/favicon.ico">
 
 <meta name="application-name" content="<?=$env["Title"]?>">
 <meta name="msapplication-TileColor" content="#000000">
