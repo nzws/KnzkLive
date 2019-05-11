a0ea00f23cccd233477e7f3d54fb912b3e5a5147
nzws <git@nzws.me>
2019-05-11 16:17:42 +0900

    change to pre-push

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,11 +4,7 @@ const tasks = arr => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks([
-      'lint-staged',
-      'pretty-quick --staged',
-      'yarn check:deps',
-      'yarn check:audit'
-    ])
+    'pre-commit': tasks(['lint-staged', 'pretty-quick --staged']),
+    'pre-push': tasks(['yarn check:deps', 'yarn check:audit'])
   }
 };
