4a9b2829f58165b6c73feae42a4b2e7e57020992
nzws <git@nzws.me>
2019-05-11 19:16:54 +0900

    Replace audit-ci to yarn audit (#177)

--- .huskyrc.js
+++ .huskyrc.js
@@ -5,6 +5,6 @@ const tasks = arr => arr.join(' && ');
 module.exports = {
   hooks: {
     'pre-commit': tasks(['lint-staged', 'pretty-quick --staged']),
-    'pre-push': tasks(['yarn check:deps', 'yarn check:audit'])
+    'pre-push': tasks(['yarn check:deps', 'yarn audit'])
   }
 };
