6a2f7f32c35b8524c1e52bd1ecca1644b1c474d9
nzws <dev@nzws.me>
2021-05-29 04:27:41 +0900

    Fix api

--- public/api/pubsv/publish.php
+++ public/api/pubsv/publish.php
@@ -2,24 +2,25 @@
 require_once "../../../lib/bootloader.php";
 require_once "../../../lib/apiloader.php";
 
-$id = strstr(s($_POST["stream"]), 'stream', true);
+$post = json_decode(file_get_contents("php://input"), true);
+$id = strstr(s($post["stream"]), 'stream', true);
 $live = getLive($id);
 if (!$live || $live["is_live"] === 0) {
     showError('Authentication failed.', 403);
 }
 
-$publishToken = str_replace('?token=', '', $_POST["param"]);
+$publishToken = str_replace('?token=', '', $post["param"]);
 
-if (strpos($_POST["stream"], 'collabo') !== false) { // collabo
-    $collabo_id = strstr(str_replace($id . 'stream', '', s($_POST["stream"])), 'collabo', true);
+if (strpos($post["stream"], 'collabo') !== false) { // collabo
+    $collabo_id = strstr(str_replace($id . 'stream', '', s($post["stream"])), 'collabo', true);
     if ($publishToken !== $live["misc"]["collabo"][$collabo_id]["token"]) {
         showError('Authentication failed.', 403);
     }
 
-    if ($_GET["mode"] === "on_publish") { //配信開始
+    if ($post["action"] === "on_publish") { //配信開始
         setCollaboLiveStatus($collabo_id, $live["id"], 2);
         api_json(["is_record" => false]);
-    } elseif ($_GET["mode"] === "on_unpublish") { //配信終了
+    } elseif ($post["action"] === "on_unpublish") { //配信終了
         setCollaboLiveStatus($collabo_id, $live["id"], 1);
     }
 } else { // main
@@ -27,9 +28,9 @@ if (strpos($_POST["stream"], 'collabo') !== false) { // collabo
         showError('Authentication failed.', 403);
     }
 
-    if ($_GET["mode"] === "on_publish") { //配信開始
+    if ($post["action"] === "on_publish") { //配信開始
         setLiveStatus($live["id"], 2);
-    } elseif ($_GET["mode"] === "on_unpublish") { //配信終了
+    } elseif ($post["action"] === "on_unpublish") { //配信終了
         $liveUser = getUser($live["user_id"]);
         if (isset($liveUser["misc"]["auto_close"]) && $liveUser["misc"]["auto_close"] && $live["is_started"] == 1) {
             end_live($live["id"]);
